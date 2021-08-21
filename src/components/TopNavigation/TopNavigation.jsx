cbb5d95b4e15385d2e5e63bb6ab488ffa338cc1a
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-08-21 12:20:26 +0600

    create scroll method for navigation bar

--- src/components/TopNavigation/TopNavigation.jsx
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -4,12 +4,33 @@ import '../../asset/css/custom.css';
 import {Navbar, Container, Nav} from 'react-bootstrap';
 
 class TopNavigation extends Component {
+
+  constructor() {
+    super();
+    this.state={
+      navbarBrand: 'navTitle'
+    }
+  }
+
+  onScrollMethod = () => {
+    if(window.scrollY > 100) {
+      this.setState({navbarBrand: 'navTitleScroll'})
+    } 
+    else if (window.scrollY < 100) {
+      this.setState({navbarBrand: 'navTitle'})
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScrollMethod)
+  }
+
   render() {
     return (
       <Fragment>
         <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">Sazzad Shakh</Navbar.Brand>
+          <Container fluid={true}>
+            <Navbar.Brand className={this.state.navbarBrand}>Sazzad Shakh</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto"></Nav>
