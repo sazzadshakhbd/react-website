791ae7f6bf6e7cddef3bdac5bbf6c2fc1e7a85ad
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-08-28 23:47:30 +0600

    add video-react js implenment on video component

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -2,6 +2,8 @@ import React, { Component, Fragment } from 'react';
 import { Container, Row, Col, Card, Modal, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
+import 'video-react/dist/video-react.css';
+import {Player, BigPlayButton} from 'video-react';
 
 class Video extends Component {
   constructor(){
@@ -34,16 +36,17 @@ class Video extends Component {
             </Col>
           </Row>
         </Container>
-        <Modal show={this.state.VideoModal} onHide={this.VideoModalClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.VideoModalClose}>
+        <Modal size="lg" show={this.state.VideoModal} onHide={this.VideoModalClose}>
+          <Modal.Body>
+            <Player  poster="https://video-react.js.org/assets/poster.png"
+              src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
+              <BigPlayButton position="center"></BigPlayButton>
+            </Player>
+          </Modal.Body>
+          <Modal.Footer className="justify-content-start">
+            <Button variant="primary" onClick={this.VideoModalClose}>
               Close
             </Button>
-           
           </Modal.Footer>
         </Modal>
       </Fragment>
