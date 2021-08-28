f1e3dc3cf4ef07cb248f4e4d790c2647937aecd1
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-08-28 06:58:22 +0600

    modal add on video component

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,9 +1,21 @@
 import React, { Component, Fragment } from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, Modal, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 
 class Video extends Component {
+  constructor(){
+    super();
+    this.state = {
+      VideoModal: false
+    }
+  }
+  VideoModalOpen=()=>{
+    this.setState({VideoModal: true})
+  }
+  VideoModalClose=()=>{
+    this.setState({VideoModal: false})
+  }
   render() {
     return (
       <Fragment>
@@ -16,12 +28,24 @@ class Video extends Component {
                     <Card.Text className="VideoCardDes">
                     First i analysis the requirement of project. According to the requirement i make a proper technical analysis, then i build a software architecture. According to the planning i start coding. Testing is also going on with coding. Final testing take place after finishing coding part. After successful implementation i provide 6 month free bug fixing service for corresponding project.
                     </Card.Text>
-                    <span><FontAwesomeIcon className="VideoPlayIcon" icon={faPlayCircle} /></span> 
+                    <span><FontAwesomeIcon onClick={this.VideoModalOpen} className="VideoPlayIcon" icon={faPlayCircle} /></span> 
                   </Card.Body>
                 </Card>
             </Col>
           </Row>
         </Container>
+        <Modal show={this.state.VideoModal} onHide={this.VideoModalClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.VideoModalClose}>
+              Close
+            </Button>
+           
+          </Modal.Footer>
+        </Modal>
       </Fragment>
     );
   }
