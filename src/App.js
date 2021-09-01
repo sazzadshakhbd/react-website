1bb160b858fedda256c6eab5a9ddfc0e36ec3e34
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-09-01 22:33:18 +0600

    create homepage and all component transfered this page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import React from "react";
-import TopBanner from "./components/TopBanner/TopBanner";
-import Services from "./components/Services/Services";
-import TopNavigation from "./components/TopNavigation/TopNavigation";
-import Analysis from "./components/Analysis/Analysis";
-import Summary from "./components/Summary/Summary";
-import RecentProject from "./components/RecentProject/RecentProject";
-import Courses from "./components/Courses/Courses";
-import Video from "./components/Video/Video";
-import Review from "./components/Review/Review";
-import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/HomePage";
 function App() {
   return (
     <div>
-      <TopNavigation></TopNavigation>
-      <TopBanner></TopBanner>
-      <Services></Services>
-      <Analysis></Analysis>
-      <Summary></Summary>
-      <RecentProject></RecentProject>
-      <Courses></Courses>
-      <Video></Video>
-      <Review></Review>
-      <Footer></Footer>
+      <HomePage></HomePage>
     </div>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,34 @@
+import React, { Component, Fragment } from 'react';
+import TopBanner from "../components/TopBanner/TopBanner";
+import Services from "../components/Services/Services";
+import TopNavigation from "../components/TopNavigation/TopNavigation";
+import Analysis from "../components/Analysis/Analysis";
+import Summary from "../components/Summary/Summary";
+import RecentProject from "../components/RecentProject/RecentProject";
+import Courses from "../components/Courses/Courses";
+import Video from "../components/Video/Video";
+import Review from "../components/Review/Review";
+import Footer from "../components/Footer/Footer";
+
+class HomePage extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <TopNavigation></TopNavigation>
+          <TopBanner></TopBanner>
+          <Services></Services>
+          <Analysis></Analysis>
+          <Summary></Summary>
+          <RecentProject></RecentProject>
+          <Courses></Courses>
+          <Video></Video>
+          <Review></Review>
+          <Footer></Footer>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default HomePage;
