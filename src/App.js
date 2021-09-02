7d15a7601db530b7cc65bd3d5510e5d12275fc82
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-09-02 06:01:39 +0600

    create about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import TopBannerAll from "./components/TopBannerAll/TopBannerAll";
-import TopNavigation from "./components/TopNavigation/TopNavigation";
+import AboutPage from "./pages/AboutPage";
+
 function App() {
   return (
     <div>
-      <TopNavigation></TopNavigation>
-      <TopBannerAll pageTitle="About Us"></TopBannerAll>
+      <AboutPage></AboutPage>
     </div>
   );
 }
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,22 @@
+import React, { Component, Fragment } from 'react';
+import AboutUsContent from "../components/AboutUsContent/AboutUsContent";
+import TopBannerAll from "../components/TopBannerAll/TopBannerAll";
+import TopNavigation from "../components/TopNavigation/TopNavigation";
+import Footer from "../components/Footer/Footer";
+
+class AboutPage extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <TopNavigation></TopNavigation>
+          <TopBannerAll pageTitle="About Me"></TopBannerAll>
+          <AboutUsContent></AboutUsContent>
+          <Footer></Footer>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default AboutPage;
