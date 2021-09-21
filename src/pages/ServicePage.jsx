f3a12e7b3beab4d6710979207a0e26ec6591ca04
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-09-21 09:20:00 +0600

    create service page

---
+++ src/pages/ServicePage.jsx
@@ -0,0 +1,22 @@
+import React, { Component, Fragment } from 'react';
+import TopBannerAll from '../components/TopBannerAll/TopBannerAll';
+import TopNavigation from '../components/TopNavigation/TopNavigation';
+import Footer from '../components/Footer/Footer';
+import Services from '../components/Services/Services'
+import ContactSection from '../components/ContactSection/ContactSection';
+
+class ServicePage extends Component {
+  render() {
+    return (
+      <Fragment>
+        <TopNavigation></TopNavigation>
+        <TopBannerAll pageTitle="My Services"></TopBannerAll>
+        <Services></Services>
+        <ContactSection></ContactSection>
+        <Footer></Footer>
+      </Fragment>
+    );
+  }
+}
+
+export default ServicePage;
