cfc4005e5aa5dcd80b761a537c05a3ccb08d8516
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-10-14 16:57:21 +0600

    css apply for course details page

--- src/components/CourseDetails/CourseDetails.jsx
+++ src/components/CourseDetails/CourseDetails.jsx
@@ -9,15 +9,15 @@ class CourseDetails extends Component {
       <Fragment>
         <Container fluid={true} className="p-0 TopBannerAllFixed">
           <div className="TopBannerAllOverlay">
-            <Container className="TopBannerContentAll">
+            <Container className="TopBannerContentAllCourse">
               <Row>
                 <Col sm={12} md={6} lg={6}>
                   <h3>Full dynamic website with admin panel</h3>
-                  <p>Total Lecture: 30</p>
-                  <p>Total Student: 40</p>
+                  <p className="mb-1 text-warning">Total Lecture: 30</p>
+                  <p className="mb-1 text-warning">Total Student: 40</p>
                 </Col>
                 <Col sm={12} md={6} lg={6}>
-                  <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga illo eveniet, odio repellendus repellat earum, iste exercitationem ea sapiente odit obcaecati dolore quae est inventore facilis qui iure saepe eum. </p>
+                  <h5> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga illo eveniet, odio repellendus repellat earum, iste exercitationem ea sapiente odit obcaecati dolore quae est inventore facilis qui iure saepe eum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga illo eveniet, odio repellendus repellat earum, iste exercitationem ea sapiente odit obcaecati dolore quae est inventore facilis qui iure saepe eum. </h5>
                 </Col>
               </Row>
             </Container>
@@ -26,14 +26,18 @@ class CourseDetails extends Component {
         <Container className="mt-5">
           <Row>
             <Col sm={12} md={6} lg={6}>
-              <h1>Skill you get</h1>
+              <h1 className="serviceCardTitle">Skill you get</h1>
               <ul>
-                <li>Unlimited Dynamic Product Category</li>
-                <li>Unlimited Dynamic Product Category</li>
-                <li>Unlimited Dynamic Product Category</li>
-                <li>Unlimited Dynamic Product Category</li>
-                <li>Unlimited Dynamic Product Category</li>
-                <li>Unlimited Dynamic Product Category</li>
+                <li className="serviceCardSubTitle">Unlimited Dynamic Product Category</li>
+                <li className="serviceCardSubTitle">Unlimited Dynamic Product Category</li>
+                <li className="serviceCardSubTitle">Unlimited Dynamic Product Category</li>
+                <li className="serviceCardSubTitle">Unlimited Dynamic Product Category</li>
+                <li className="serviceCardSubTitle">Unlimited Dynamic Product Category</li>
+                <li className="serviceCardSubTitle">Unlimited Dynamic Product Category</li>
+                <li className="serviceCardSubTitle">Unlimited Dynamic Product Category</li>
+                <li className="serviceCardSubTitle">Unlimited Dynamic Product Category</li>
+                <li className="serviceCardSubTitle">Unlimited Dynamic Product Category</li>
+                
               </ul>
               <Button variant="primary">Details</Button>
             </Col>
